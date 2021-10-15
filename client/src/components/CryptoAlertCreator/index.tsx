bdb82acfbc17142ed73701545f5c9dfde0bd36a0
Chris <Kirch2@gmail.com>
2021-10-14 22:10:34 -0400

    added cryptoalertcreator

---
+++ client/src/components/CryptoAlertCreator/index.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+
+interface CryptoAlertCreatorProps {
+  children: (childProps: {
+    loading: boolean;
+    createCryptoAlert: (cryptoAlert: any) => void;
+  }) => React.ReactNode;
+}
+
+//used to create to new newCryptoAlert
+export function CryptoAlertCreator(props: CryptoAlertCreatorProps) {
+  const history = useHistory();
+  const [loading, setLoading] = useState(false);
+
+  function createCryptoAlert(newCryptoAlert: any) {
+    setLoading(true);
+    fetch(`/api/crypto_alerts`, {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newCryptoAlert),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setLoading(false);
+        history.push(`/crypto_alerts/${data.id}`);
+      })
+      .catch(() => {
+        setLoading(false);
+      });
+  }
+
+  return (
+    <React.Fragment>
+      {props.children({
+        loading,
+        createCryptoAlert,
+      })}
+    </React.Fragment>
+  );
+}
