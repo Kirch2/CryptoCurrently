c00ab42d9425645c84d57cf51f2ee8c7bc1730c9
Chris <Kirch2@gmail.com>
2021-10-17 19:44:42 -0400

    Updated crypto sync job

--- app/jobs/cryptocurrency_sync_job.rb
+++ app/jobs/cryptocurrency_sync_job.rb
@@ -1,20 +1,11 @@
 class CryptocurrencySyncJob < ApplicationJob
   queue_as :default
 
-  def after_perform
-    puts "re-queue sync job"
-    puts "re-queue sync job"
-    puts "re-queue sync job"
-    puts "re-queue sync job"
-    puts "re-queue sync job"
-    puts "re-queue sync job"
-    # CryptocurrencySyncJob.set(wait: 2.minutes).perform_later
-    # CryptocurrencySyncJob.set(wait: 30.seconds).perform_later
-  end
-
   def perform(*args)
-    # Cryptocurrency.sync_all_coinmarketcap
-    puts "SYNC SHIT"
+    Cryptocurrency.sync_all_coinmarketcap
+    sleep 120
+    puts "re-queue sync job"
+    CryptocurrencySyncJob.perform_now
   end
 
 end
