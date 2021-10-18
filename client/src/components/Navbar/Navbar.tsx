055d519a0bd98044c3afea9eb6de46eb75e10e6c
Chris <Kirch2@gmail.com>
2021-10-17 23:17:37 -0400

    updated Css/name

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -16,7 +16,7 @@ export function Navbar(props: NavBarProps) {
           to="/cryptocurrencies"
           className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none"
         >
-          <span className="fs-2 text-white">CurrentCrypto</span>
+          <span className="fs-2 text-white">CryptoCurrently</span>
         </NavLink>
         <ul className="nav nav-pills">
           <li className="nav-item">
