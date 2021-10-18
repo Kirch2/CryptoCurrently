5964917aedcb5fa5c8359842338fc0a0e5709486
Chris <Kirch2@gmail.com>
2021-10-17 20:07:33 -0400

    Disabled broken sync code

--- app/jobs/cryptocurrency_sync_job.rb
+++ app/jobs/cryptocurrency_sync_job.rb
@@ -3,9 +3,10 @@ class CryptocurrencySyncJob < ApplicationJob
 
   def perform(*args)
     Cryptocurrency.sync_all_coinmarketcap
-    sleep 30
-    puts "re-queue sync job"
-    CryptocurrencySyncJob.perform_now
+    # THIS DOESN'T WORK :(
+    # sleep 30
+    # puts "re-queue sync job"
+    # CryptocurrencySyncJob.perform_now
   end
 
 end
