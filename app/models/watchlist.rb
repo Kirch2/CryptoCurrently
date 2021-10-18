ce9faff2b251d8df827fe56a88064fa3b94032c8
Chris <Kirch2@gmail.com>
2021-10-17 20:55:14 -0400

    added self.watchlistalert

--- app/models/watchlist.rb
+++ app/models/watchlist.rb
@@ -1,9 +1,11 @@
 class Watchlist < ApplicationRecord
   belongs_to :user
   has_many :watchlist_entries
+  has_many :watchlist_alerts
   validates :label, :description, presence: true
   before_destroy :destroy_watchlist_entries
   def destroy_watchlist_entries
      self.watchlist_entries.destroy_all
+     self.watchlist_alerts.destroy_all
   end   
 end
