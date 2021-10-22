8ce88330d3ea0626b6e4eda8c1524101b37f0d92
Chris <Kirch2@gmail.com>
2021-10-22 14:22:11 -0400

    added user to profile page

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -1,5 +1,19 @@
 import React from "react";
-
+import { Loading } from "../components/Loading";
+import { UserIdFetcher } from "../components/UserIdFetcher";
 export function Profile() {
-  return <h1>Profile</h1>;
+  const user_id = Number(localStorage.getItem("user_id"));
+  return (
+    <div>
+      <h1>Profile</h1>
+      <UserIdFetcher id={user_id}>
+        {({ user, loading }) => {
+          if (loading) {
+            return <Loading />;
+          }
+          return <pre>{JSON.stringify(user, null, 2)}</pre>;
+        }}
+      </UserIdFetcher>
+    </div>
+  );
 }
