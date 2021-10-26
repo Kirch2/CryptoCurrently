8e29e6e8e541cfbfab99c14309141d1dcd645a42
Chris <Kirch2@gmail.com>
2021-10-25 22:45:54 -0400

    more links

--- client/src/pages/Resources.tsx
+++ client/src/pages/Resources.tsx
@@ -1,5 +1,51 @@
 import React from "react";
 
 export function Resources() {
-  return <h1>Resources</h1>;
+  return (
+    <div>
+      <h1>Resources</h1>
+      <ul>
+        <li>
+          <a
+            className="bi bi-box-arrow-right"
+            href="https://www.coindesk.com/"
+            target="_blank"
+            style={{ textDecoration: "none" }}
+          >
+            CoinDesk
+          </a>
+        </li>
+        <li>
+          <a
+            className="bi bi-box-arrow-right"
+            href="https://nydig.com/research/bitcoin-a-peer-to-peer-electronic-cash-system"
+            target="_blank"
+            style={{ textDecoration: "none" }}
+          >
+            Bitcoin: A Peer-to-Peer Electronic Cash System
+          </a>
+        </li>
+        <li>
+          <a
+            className="bi bi-box-arrow-right"
+            href="https://google.com"
+            target="_blank"
+            style={{ textDecoration: "none" }}
+          >
+            Google
+          </a>
+        </li>
+        <li>
+          <a
+            className="bi bi-box-arrow-right"
+            href="https://google.com"
+            target="_blank"
+            style={{ textDecoration: "none" }}
+          >
+            Google
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 }
