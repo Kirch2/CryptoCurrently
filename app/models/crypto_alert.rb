7d020f49e9cbcbb3d6bc85cb65f5dcc0cbc7a97d
Chris <Kirch2@gmail.com>
2021-10-27 14:00:00 -0400

    number helper for email

--- app/models/crypto_alert.rb
+++ app/models/crypto_alert.rb
@@ -1,4 +1,5 @@
 class CryptoAlert < ApplicationRecord
+  include ActionView::Helpers::NumberHelper
   belongs_to :cryptocurrency
   belongs_to :user
 
@@ -18,7 +19,7 @@ class CryptoAlert < ApplicationRecord
     if (self.should_send_email)
       begin
         # TODO - update message or subject to include price
-        CryptoAlertMailer.send_email(email: self.user.email, subject: "CryptoCurrently - " + self.cryptocurrency.label + ": $" + self.cryptocurrency.price.to_s, message: "Sent with CryptoCurrently.app").deliver
+        CryptoAlertMailer.send_email(email: self.user.email, subject: "CryptoCurrently - " + self.cryptocurrency.label + ": $" + number_to_currency(self.cryptocurrency.price), message: "Sent with CryptoCurrently.app").deliver
       rescue => e
         puts "Email Send Failed"
         puts e
